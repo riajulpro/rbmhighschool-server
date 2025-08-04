535d689184a4a8b512f33b37019ced8a0022537e
RiaJul Pro <riajulpro@outlook.com>
2025-08-04 16:34:37 +0600

    route fixed

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use("/api/notices", require("./routes/notice.route").default);
 app.use("/api/posts", require("./routes/post.route").default);
 app.use("/api/routine", require("./routes/routine.route").default);
 app.use("/api/facilities", require("./routes/facility.route").default);
-app.use("/api/vacancies", require("./routes/vacancies.route").default);
+app.use("/api/vacancies", require("./routes/vacancy.route").default);
 app.use("/api/vacations", require("./routes/vacation.route").default);
 app.use("/api/authorities", require("./routes/authority.route").default);
 app.use("/api/staffs", require("./routes/staff.route").default);
