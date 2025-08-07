1ce71db379e3f000f5a15bc019a0d77adda56eca
RiaJul Pro <riajulpro@outlook.com>
2025-08-07 11:19:10 +0600

    Updated!

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -28,7 +28,7 @@ router.post("/forgot-password", forgotPassword);
 router.post("/verify-otp", verifyOtp);
 router.post("/change-password", authenticate, changePassword);
 router.put("/update/:id", authenticate, updateUser);
-router.put("/update/:email", authenticate, updateUserWithEmail);
+router.put("/update/email/:email", authenticate, updateUserWithEmail);
 router.delete(
   "/remove/:id",
   authenticate,
